1862620d509d0ea8bbfd1477dddc158997c0a601
Essoly Elyazid <65821915+MrElyazid@users.noreply.github.com>
2025-02-03 14:26:06 +0100

    all keys revoked

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnb8sSIQB9nyTkUf7yyJBNqXA8vsBhs-s",
-  authDomain: "blossom-77ec5.firebaseapp.com",
-  projectId: "blossom-77ec5",
-  storageBucket: "blossom-77ec5.appspot.com",
-  messagingSenderId: "224118477905",
-  appId: "1:224118477905:web:b2ba1798b0da3a978fd470",
-  measurementId: "G-TEZJTW8B4K",
+  // Everything is revoked, sigh, we shouldve used a .env file.
 };
 
 const app = initializeApp(firebaseConfig);
